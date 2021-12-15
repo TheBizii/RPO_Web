396d9985ce68ec20cd5be1fea6019c8440d5cd4a
profek10 <aleksander.grobelnik1@gmail.com>
2021-12-15 22:13:35 +0100

    New fancy calculation logic

--- src/endpoints/postNearbyShops.js
+++ src/endpoints/postNearbyShops.js
@@ -5,10 +5,23 @@ const router = express.Router()
 const params = {
   provider: 'openstreetmap'
 }
+// const Post = require('./modules/orm/models/post')
 const geoCoder = nodeGeo(params)
 
+function measure (lat1, lon1, lat2, lon2) {
+  const cLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180
+  const cLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180
+  const a = Math.sin(cLat / 2) * Math.sin(cLat / 2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(cLon / 2) * Math.sin(cLon / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  const d = 6378.137 * c
+  return d * 1000
+}
 // <a href="https://my-location.org/?lat=46.5554&lng=15.6465" target="_blank">(46.5554,15.6465)</a>
 router.post('/nearbyShops', async function (req, res) {
+  // const posts = await Post.readAll()
+  // res.send(posts)
   res.setHeader('Content-Type', 'application/json')
   if (req.body.lat === undefined && req.body.lng === undefined) {
     res.json({
@@ -22,10 +35,16 @@ router.post('/nearbyShops', async function (req, res) {
     const keys = Object.keys(result)
     const resArr = []
     for (let i = 0; i < keys.length; i++) {
-      const retObj = await geoCoder.geocode(result[keys[i]].address)
-      console.log(result[keys[i]])
-      resArr.push({ lat: retObj[0].latitude, lon: retObj[0].longitude, dis: Math.abs(retObj[0].latitude - req.body.lat) + Math.abs(retObj[0].longitude - req.body.lng), name: result[keys[i]].title })
+      let coord = []
+      if (result[keys[i]].coordinate === null) {
+        coord = await geoCoder.geocode(`${result[keys[i]].address}, ${result[keys[i]].city} ${result[keys[i].country]}`)
+        db.updateCoordinates(result[keys[i]].id, coord[0].latitude, coord[0].longitude)
+      } else {
+        coord.push({ latitude: result[keys[i]].coordinate.x, longitude: result[keys[i]].coordinate.y })
+      }
+      resArr.push({ lat: coord[0].latitude, lon: coord[0].longitude, dis: Math.floor(measure(coord[0].latitude, coord[0].longitude, req.body.lat, req.body.lng) * 100) / 100, name: result[keys[i]].title })
     }
+    resArr.sort((a, b) => a.dis - b.dis)
     res.json({
       shops: resArr
     })
--- src/modules/db.js
+++ src/modules/db.js
@@ -49,7 +49,21 @@ async function getAllShops () {
   try {
     await connectToDB()
     const query = await promisify(connection.query).bind(connection)
-    const result = await query('SELECT a.address, a.coordinate, pl.title FROM address a JOIN partner_location pl ON pl.address_id = a.ID')
+    const result = await query('SELECT a.id, a.address, a.coordinate, pl.title, p.city_name as city, c.name as country FROM address a JOIN partner_location pl ON pl.address_id = a.ID JOIN post p ON a.post_id = p.ID JOIN country c ON p.country_id = c.ID')
+    if (result.length === 0) {
+      return null
+    }
+    return JSON.stringify(result)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function updateCoordinates (id, lat, lng) {
+  try {
+    await connectToDB()
+    const query = await promisify(connection.query).bind(connection)
+    const result = await query(`UPDATE address SET coordinate = POINT(${lat}, ${lng}) WHERE ID = ${id}`)
     if (result.length === 0) {
       return null
     }
@@ -74,5 +88,6 @@ module.exports = {
   getAllCountries,
   confirmLoginInformation,
   getAllShops,
+  updateCoordinates,
   query
 }
