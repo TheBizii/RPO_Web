3c2a3e6efdf46949a621f3d0067f87f544d739ff
profek10 <aleksander.grobelnik1@gmail.com>
2022-01-13 11:54:33 +0100

    Fixed syntax error in orm model

--- src/modules/orm/models/order.js
+++ src/modules/orm/models/order.js
@@ -1,127 +1,127 @@
-const db = require('../../db');
-const model = require('../model');
-const User = require('./user'):
+const db = require('../../db')
+const model = require('../model')
+const User = require('./user')
 
-const Model = model.Model;
+const Model = model.Model
 
 class Order extends Model {
-  constructor() {
-    super();
+  constructor () {
+    super()
   }
 
-  setDate(date) {
-    this.date = date;
+  setDate (date) {
+    this.date = date
   }
 
-  setDeliverer(deliverer) {
-    if(deliverer.constructor === User) {
-      this.deliverer = deliverer;
+  setDeliverer (deliverer) {
+    if (deliverer.constructor === User) {
+      this.deliverer = deliverer
     } else {
-      throw new Error(`Got ${deliverer.constructor}, expected object of type User.`);
-    }
-  }
-  
-  setCustomer(customer) {
-    if(customer.constructor === User) {
-      this.customer = customer;
-    } else {
-      throw new Error(`Got ${customer.constructor}, expected object of type User.`);
+      throw new Error(`Got ${deliverer.constructor}, expected object of type User.`)
     }
   }
 
-  setTotalValue(totalValue) {
-    this.totalValue = totalValue;
+  setCustomer (customer) {
+    if (customer.constructor === User) {
+      this.customer = customer
+    } else {
+      throw new Error(`Got ${customer.constructor}, expected object of type User.`)
+    }
   }
 
-  setTotalDiscountPercentage(totalDiscountPercentage) {
-    this.totalDiscountPercentage = totalDiscountPercentage;
+  setTotalValue (totalValue) {
+    this.totalValue = totalValue
   }
 
-  setValueWithDiscount(valueWithDiscount) {
-    this.valueWithDiscount = valueWithDiscount;
+  setTotalDiscountPercentage (totalDiscountPercentage) {
+    this.totalDiscountPercentage = totalDiscountPercentage
   }
-  
-  addCoupon(coupon) {
+
+  setValueWithDiscount (valueWithDiscount) {
+    this.valueWithDiscount = valueWithDiscount
+  }
+
+  addCoupon (coupon) {
     if (this.coupons === undefined) {
-      this.coupons = [];
+      this.coupons = []
     }
 
-    if (this.coupons.includes(coupon)) return;
+    if (this.coupons.includes(coupon)) return
 
-    this.coupons.push(coupon);
+    this.coupons.push(coupon)
   }
 
-  removeCoupon(coupon) {
-    if (this.coupons === undefined) return;
+  removeCoupon (coupon) {
+    if (this.coupons === undefined) return
 
-    this.coupons = this.coupons.filter(c => c !== coupon);
+    this.coupons = this.coupons.filter(c => c !== coupon)
   }
 
-  getDate() {
-    return this.date;
+  getDate () {
+    return this.date
   }
 
-  getDeliverer() {
-    return this.deliverer;
+  getDeliverer () {
+    return this.deliverer
   }
 
-  getCustomer() {
-    return this.customer;
+  getCustomer () {
+    return this.customer
   }
 
-  getTotalValue() {
-    return this.totalValue;
+  getTotalValue () {
+    return this.totalValue
   }
 
-  getTotalDiscountPercentage() {
-    return this.totalDiscountPercentage;
+  getTotalDiscountPercentage () {
+    return this.totalDiscountPercentage
   }
 
-  getValueWithDiscount() {
-    return this.valueWithDiscount;
+  getValueWithDiscount () {
+    return this.valueWithDiscount
   }
 
-  getCoupons() {
-    return this.coupons;
+  getCoupons () {
+    return this.coupons
   }
 
-  async create() {
+  async create () {
     try {
-      this.setActive(true);
-      let sql = `INSERT INTO order (date, deliverer_id, customer_id, total_value, total_discount_percentage, value_with_discount, active) VALUES ("${ this.getDate() }", "${ this.getDeliverer().getID() }", "${ this.getCustomer().getID() }", "${ this.getTotalValue() }", "${ this.getTotalDiscountPercentage() }", "${ this.getValueWithDiscount() }", 1);`;
-      let res = await db.query(sql);
-      this.setID(res.insertId);
-      
-      for(let i = 0; i < this.getCoupons().length; i++) {
-        const couponsql = `INSERT INTO used_coupons (order_id, coupon_id, active) VALUES (${ this.getID() }, ${ this.getCoupons()[i] }, 1);`;
-        await db.query(couponsql);
+      this.setActive(true)
+      const sql = `INSERT INTO order (date, deliverer_id, customer_id, total_value, total_discount_percentage, value_with_discount, active) VALUES ("${this.getDate()}", "${this.getDeliverer().getID()}", "${this.getCustomer().getID()}", "${this.getTotalValue()}", "${this.getTotalDiscountPercentage()}", "${this.getValueWithDiscount()}", 1);`
+      const res = await db.query(sql)
+      this.setID(res.insertId)
+
+      for (let i = 0; i < this.getCoupons().length; i++) {
+        const couponsql = `INSERT INTO used_coupons (order_id, coupon_id, active) VALUES (${this.getID()}, ${this.getCoupons()[i]}, 1);`
+        await db.query(couponsql)
       }
-      
-      return res;
-    } catch(err) {
-      throw new Error(err);
+
+      return res
+    } catch (err) {
+      throw new Error(err)
     }
-    return null;
+    return null
   }
 
-  async read(id) {
+  async read (id) {
     try {
-      let sql = `SELECT * FROM order WHERE ID = ${ id } AND active <> 0;`;
-      let res = await db.query(sql);
-      if(res.length > 0) {
-        let order = res[0];
-        let deliverer = new User();
-        let customer = new User();
-        await deliverer.read(order.deliverer_id);
-        await customer.read(order.customer_id);
-        this.setID(order.ID);
-        this.setDate(order.date);
-        this.setDeliverer(deliverer);
-        this.setCustomer(customer);
-        this.setTotalValue(order.total_value);
-        this.setTotalDiscountPercentage(order.total_discount_percentage);
-        this.setValueWithDiscount(order.value_with_discount);
-        this.setActive(order.active);
+      const sql = `SELECT * FROM order WHERE ID = ${id} AND active <> 0;`
+      const res = await db.query(sql)
+      if (res.length > 0) {
+        const order = res[0]
+        const deliverer = new User()
+        const customer = new User()
+        await deliverer.read(order.deliverer_id)
+        await customer.read(order.customer_id)
+        this.setID(order.ID)
+        this.setDate(order.date)
+        this.setDeliverer(deliverer)
+        this.setCustomer(customer)
+        this.setTotalValue(order.total_value)
+        this.setTotalDiscountPercentage(order.total_discount_percentage)
+        this.setValueWithDiscount(order.value_with_discount)
+        this.setActive(order.active)
 
         // Load coupons
         const couponsql = `SELECT coupon_id FROM used_coupons WHERE order_id="${order.ID}" AND active <> 0;`
@@ -129,38 +129,38 @@ class Order extends Model {
         for (let i = 0; i < couponsRes.length; i++) {
           this.addCoupon(couponsRes[i].coupon_id)
         }
-        
-        return this;
+
+        return this
       }
-    } catch(err) {
-      throw new Error(err);
+    } catch (err) {
+      throw new Error(err)
     }
 
-    return null;
+    return null
   }
 
-  static async readAll() {
+  static async readAll () {
     try {
-      let sql = `SELECT ID FROM order WHERE active <> 0`;
-      let res = await db.query(sql);
-      let orders = [];
-      for(let i = 0; i < res.length; i++) {
-        let order = new Order();
-        await order.read(res[i].ID);
-        orders.push(order);
+      const sql = 'SELECT ID FROM order WHERE active <> 0'
+      const res = await db.query(sql)
+      const orders = []
+      for (let i = 0; i < res.length; i++) {
+        const order = new Order()
+        await order.read(res[i].ID)
+        orders.push(order)
       }
-      return orders;
-    } catch(err) {
-      throw new Error(err);
+      return orders
+    } catch (err) {
+      throw new Error(err)
     }
 
-    return null;
+    return null
   }
 
-  async update() {
+  async update () {
     try {
-      let sql = `UPDATE order SET date="${ this.getDate() }", deliverer_id="${ this.getDeliverer().getID() }", customer_id="${ this.getCustomer().getID() }", total_value="${ this.getTotalValue() }" total_discount_percentage="${ this.getTotalDiscountPercentage() }", value_with_discount="${ this.getValueWithDiscount() }", active="${ this.getActive() }" WHERE ID=${ this.getID() };`;
-      let res = await db.query(sql);
+      const sql = `UPDATE order SET date="${this.getDate()}", deliverer_id="${this.getDeliverer().getID()}", customer_id="${this.getCustomer().getID()}", total_value="${this.getTotalValue()}" total_discount_percentage="${this.getTotalDiscountPercentage()}", value_with_discount="${this.getValueWithDiscount()}", active="${this.getActive()}" WHERE ID=${this.getID()};`
+      const res = await db.query(sql)
 
       // Check if any coupons have to be deleted
       const couponsql = `SELECT coupon_id FROM used_coupons WHERE order_id="${this.getID()}" AND active <> 0;`
@@ -177,16 +177,16 @@ class Order extends Model {
         await db.query(remcouponsql)
       }
 
-      return JSON.stringify(res);
-    } catch(err) {
-      throw new Error(err);
+      return JSON.stringify(res)
+    } catch (err) {
+      throw new Error(err)
     }
   }
 
-  async del() {
-    this.setActive(false);
-    await this.update();
+  async del () {
+    this.setActive(false)
+    await this.update()
   }
 }
 
-module.exports = Order;
+module.exports = Order
